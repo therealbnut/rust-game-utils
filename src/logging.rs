27ba6e5bdf2e409d2910deec4cde66bdde48a641
therealbnut <635596+therealbnut@users.noreply.github.com>
2023-06-01 15:16:23 +1000

    Add macroquad feature flag

--- src/logging.rs
+++ src/logging.rs
@@ -1,7 +1,5 @@
 use alloc::{borrow::Cow, string::ToString, vec::Vec};
 
-use macroquad::prelude::*;
-
 #[doc(hidden)]
 #[macro_export]
 macro_rules! log {
@@ -34,15 +32,15 @@ pub fn _log_args(
 
 pub struct Log {
     entries: Vec<LogEntry>,
-    params: TextParams,
-    fade_duration: f64,
+    #[cfg(feature = "macroquad-render")]
+    render: macroquad_render::Render,
 }
 impl Log {
     pub fn new() -> Self {
         Self {
             entries: Vec::new(),
-            params: TextParams::default(),
-            fade_duration: 0.3,
+            #[cfg(feature = "macroquad-render")]
+            render: Default::default(),
         }
     }
     pub fn update(&mut self, dt: f64) {
@@ -68,24 +66,14 @@ impl Log {
             self.entries.push(entry);
         }
     }
+
+    pub fn strings(&self) -> impl Iterator<Item = &str> {
+        self.entries.iter().map(|e| e.string.as_ref())
+    }
+
+    #[cfg(feature = "macroquad-render")]
     pub fn display(&self) {
-        let mut pos = [0.0; 2];
-        for entry in &self.entries {
-            let mut params = self.params;
-            let size = measure_text(
-                &entry.string,
-                Some(params.font),
-                params.font_size,
-                params.font_scale,
-            );
-            pos[1] += size.offset_y * 1.2;
-
-            let fade_mul = entry.timeout.min(self.fade_duration) / self.fade_duration;
-            params.color.a *= fade_mul as f32;
-            draw_text_ex(&entry.string, pos[0] + 10.0, pos[1], params);
-
-            pos[0] = 0.0;
-        }
+        self.render.display(&self.entries);
     }
 }
 
@@ -93,4 +81,46 @@ struct LogEntry {
     string: Cow<'static, str>,
     timeout: f64,
     id: Option<&'static str>,
+}
+
+#[cfg(feature = "macroquad-render")]
+mod macroquad_render {
+    use macroquad::prelude::*;
+
+    use super::LogEntry;
+
+    pub(super) struct Render {
+        fade_duration: f64,
+        params: TextParams,
+    }
+    impl Default for Render {
+        fn default() -> Self {
+            Self {
+                fade_duration: 0.3,
+                params: TextParams::default(),
+            }
+        }
+    }
+
+    impl Render {
+        pub fn display(&self, entries: &[LogEntry]) {
+            let mut pos = [0.0; 2];
+            for entry in entries {
+                let mut params = self.params;
+                let size = measure_text(
+                    &entry.string,
+                    Some(params.font),
+                    params.font_size,
+                    params.font_scale,
+                );
+                pos[1] += size.offset_y * 1.2;
+
+                let fade_mul = entry.timeout.min(self.fade_duration) / self.fade_duration;
+                params.color.a *= fade_mul as f32;
+                draw_text_ex(&entry.string, pos[0] + 10.0, pos[1], params);
+
+                pos[0] = 0.0;
+            }
+        }
+    }
 }
